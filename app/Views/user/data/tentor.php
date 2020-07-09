16b172295d53823a8acd80c6beb863dbd9aeb136
MC_XT <xcloud1612@gmail.com>
2020-07-09 19:17:51 +0700

    fix typo

--- app/Views/user/data/tentor.php
+++ app/Views/user/data/tentor.php
@@ -6,7 +6,7 @@
         <div class="container-fluid">
             <!-- Page Heading -->
             <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                <h1 class="h3 mb-0 text-gray-800">Data Tentor</h1>
                 <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-plus fa-sm text-white-50"></i> Tambah</a>
             </div>
 
@@ -28,20 +28,20 @@
                             <?php
                             $i = 1;
                             foreach ($user as $u) : ?>
-                            <tr>
-                                <td><?= $i ?></td>
-                                <td><?= $u['nama'] ?></td>
-                                <td><?= $u['username'] ?></td>
-                                <td><?= $u['email'] ?></td>
-                                <td><?= $u['jenis_kelamin'] ?></td>
-                                <td>
-                                    <center>
-                                        <button class="btn btn-info btn-sm">
-                                            <i class="fas fa-info-circle"></i>
-                                        </button>
-                                    </center>
-                                </td>
-                            </tr>
+                                <tr>
+                                    <td><?= $i ?></td>
+                                    <td><?= $u['nama'] ?></td>
+                                    <td><?= $u['username'] ?></td>
+                                    <td><?= $u['email'] ?></td>
+                                    <td><?= $u['jenis_kelamin'] ?></td>
+                                    <td>
+                                        <center>
+                                            <button class="btn btn-info btn-sm">
+                                                <i class="fas fa-info-circle"></i>
+                                            </button>
+                                        </center>
+                                    </td>
+                                </tr>
                             <?php
                                 $i++;
                             endforeach;
