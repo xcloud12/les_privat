17d72115780d841fda73e77ad4a52140d0b85ddf
MC_XT <xcloud1612@gmail.com>
2020-07-29 03:02:02 +0700

    memperbaiki tampilan home

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -49,11 +49,12 @@
             <div class="row">
                 <div class="col-sm-8 pt-3">
                     <h2 class="pb-3 display-4">Selamat Datang di LPPK Universitas Nurul Jadid</h2>
-                    <h3 class="mt-3">Visi</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis minima ipsam excepturi illo non cumque maiores cum facilis. Nemo repudiandae ut at quis magni consectetur doloremque, nihil maxime perspiciatis sequi?</p>
-
-                    <h3 class="mt-2">Misi</h3>
-                    <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia tempore quisquam, aliquam iste quas dolor quo eius officiis? Impedit vel rem ab accusamus. Animi voluptatum placeat aspernatur ad, fugiat hic.</p>
+                    <div class="mt-2 font-weight-light font-italic pb-5">Sebuah Lembaga Pengembangan Profesionalitas dan Kewirausahaan yang di kembangkan oleh Universitas Nurul Jadid untuk mengembangkan minat dan kemampuan tiap individu </div>
+                    <!-- <ul style="list-style-type: circle;" class="pb-4"> -->
+                    <h5 class="pt-2 font-italic font-weight-light"> Mengembangkat Minat Belajar</h5>
+                    <h5 class="pt-2 font-italic font-weight-light"> Menyediakan Tenaga Mengajar Yang Profesional</h5>
+                    <h5 class="pt-2 font-italic font-weight-light"> Layanan Terbaik Untuk Peserta Didik</h5>
+                    <!-- </ul> -->
                 </div>
                 <div class="col-sm-4 pt-5">
                     <form action="/" method="POST" class="user">
@@ -94,60 +95,127 @@
         </div>
     </div>
     <div class="container text-white">
-        <div class="row justify-content-center mt-5 pt-5">
-            <div class="col-5 ">
-                <p data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, iure tempora earum cupiditate vel aspernatur quo debitis minus ipsum repellendus sequi pariatur, provident perspiciatis sed dolorem! Exercitationem reprehenderit eos nesciunt!
-                </p>
-                <hr class="pt-5 pb-5">
-                <p data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, iure tempora earum cupiditate vel aspernatur quo debitis minus ipsum repellendus sequi pariatur, provident perspiciatis sed dolorem! Exercitationem reprehenderit eos nesciunt!
-                </p>
-                <hr class="pt-5 pb-5">
-                <p data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, iure tempora earum cupiditate vel aspernatur quo debitis minus ipsum repellendus sequi pariatur, provident perspiciatis sed dolorem! Exercitationem reprehenderit eos nesciunt!
-                </p>
+        <div class="row">
+            <div class="col-sm-10">
+                <h4 data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">Peserta</h4>
+                <ol class="decimal pb-3">
+                    <li class="font-weight-bold" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">Cara Membuat Akun ?</li>
+                    <p class="font-weight-normal" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">Langkah langkah melakukan pembuatan akun peserta adalah :</p>
+                    <ol class="decimal mt-0" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
+                        <li style="margin-top: -12px">Pada halaman awal pilihlah belum punya akun</li>
+                        <li>Isi data diri yang dibutuhkan</li>
+                        <li>Tekan tombol buat akun peserta</li>
+                        <li style="margin-bottom: 10px;">Akun siap untuk digunakan</li>
+                    </ol>
+                    <li class="font-weight-bold" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">Apakah Pelayanan Bagus ?</li>
+                    <p class="font-weight-normal" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">Pelayanan kami usahakan terbaik untuk pelanggan dan mementingkan kenyamanan pelanggan.</p>
+                    <li class="font-weight-bold" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">Apakah Tenaga Pengajar Berkualitas ?</li>
+                    <p class="font-weight-normal" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">Untuk tenaga pengajar atau tentor kami sediakan sudah memiliki sertifikat dan dapat di jamin kualitasnya.</p>
+                </ol>
+                <h4 data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">Tentor</h4>
+                <ol class="decimal">
+                    <li data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">Cara Mendaftar Akun</li>
+                    <p class="font-weight-normal" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">Langkah langkah melakukan pembuatan akun tentor adalah :</p>
+                    <ol class="decimal mt-0" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
+                        <li style="margin-top: -12px">Pada halaman awal pilihlah belum punya akun</li>
+                        <li>Di bawah tombol daftar sebagai murid terdapat pilihan unttuk mendaftar sebagai tentor</li>
+                        <li>Isi data diri yang dibutuhkan</li>
+                        <li>Tekan tombol buat akun tentor</li>
+                        <li style="margin-bottom: 10px;">Akun siap untuk digunakan</li>
+                    </ol>
+                    <li data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">Syarat Menjadi Tentor</li>
+                    <p class="font-weight-normal" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">Berikut adalah syarat menjadi tentor :</p>
+                    <ol class="decimal mt-0" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
+                        <li style="margin-top: -12px">Harus sabar dalam mengajar peserta</li>
+                        <li>Lulus ujian dalam materi pelajaran yang ingin di ajar</li>
+                        <li>Tekun dan ulet dalam mengajar</li>
+                        <li style="margin-bottom: 10px;">Harus niat</li>
+                    </ol>
+                    <!-- <li>Cara Melakukan Pengajuan Mengajar</li>
+                    <p class="font-weight-normal">Berikut adalah syarat menjadi tentor :</p>
+                    <ol class="decimal mt-0">
+                        <li style="margin-top: -12px">Harus sabar dalam mengajar peserta</li>
+                        <li>Lulus ujian dalam materi pelajaran yang ingin di ajar</li>
+                        <li>Tekun dan ulet dalam mengajar</li>
+                        <li style="margin-bottom: 10px;">Harus niat</li> -->
+                </ol>
+                </ol>
+            </div>
+        </div>
+
+    </div>
+    <div class="container text-white">
+        <div class="row justify-content-center mt-0 pt-5">
+            <div class="col-lg-7 col-sm-7 ">
+                <h5 data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">Kata Kata Penyemangat :</h5>
+                <h6 data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
+                    <p class="font-weight-bold mb-1">KH. Zaini Mun'im:</p>
+                    <p class="font-weight-light font-italic pb-3">"Orang yang hidup di Indonesia kemudian tidak melakukan perjuangan, dia telah berbuat maksiat. Orang yang hanya memikirkan masalah pendidikannya sendiri, maka orang itu telah berbuat maksiat. Kita semua harus memikirkan perjuangan rakyat banyak."</p>
+                    <hr class="pt-2">
+                </h6>
+                <h6 data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
+                    <p class="font-weight-bold mb-1">Muhammad Taufiq Hidayatullah</p>
+                    <p class="font-weight-light font-italic pb-3">"Kelemahan terbesarmu adalah ketika kamu menyerah dan kehebatan terbesarmu adalah ketika kamu mencoba sekali lagi."</p>
+                    <hr class="pt-2">
+                </h6>
+                <h6 data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
+                    <p class="font-weight-bold mb-1">KH. Abdul Hamid Wahid, M. Ag. (Rektor UNUJA):</p>
+                    <p class="font-weight-light font-italic pb-3">"Mari kita bersama-sama menjadi insan-insan yg unggul, inovatif dan berkeadaban. Kita hadir bukan hanya sekedar pelengkap, tetapi kita mampu memberikan warna, mampu memberikan kontribusi, dengan hadir sepenuhnya dan seutuhnya."</p>
+                </h6>
             </div>
         </div>
     </div>
     <br>
 
-    <div>
-        <div class="row">
-            <div class="col-12 col-sm-6 col-md-3 text-center " data-aos="zoom-in-up" data-aos-duration="1000">
-                <a href="https://twitter.com/@unuja" target="blank" class="text-decoration-none">
-                    <span style="font-size: 40px; color: aliceblue;"><i class="fab fa-twitter-square"></i></span>
-                    <p class="text-white ">
-                        @Unuja.ac.id
-                    </p>
-                </a>
-            </div>
-            <div class="col-12 col-sm-6 col-md-3 text-center" data-aos="zoom-in-up" data-aos-duration="1000">
-                <a href="https://www.youtube.com/channel/UCq-VDezP1Vj4pEOYiPn3Qxw" target="blank" class="text-decoration-none">
-                    <span style="font-size: 40px; color: aliceblue;"><i class="fab fa-youtube"></i></span>
-                    <p class="text-white ">
-                        Universitas Nurul Jadid
-                    </p>
-                </a></div>
-            <div class="col-12 col-sm-6 col-md-3 text-center" data-aos="zoom-in-up" data-aos-duration="1000">
-                <a href="https://web.facebook.com/universitasnuruljadid" target="blank" class="text-decoration-none">
-                    <span style="font-size: 40px; color: aliceblue;"><i class="fab fa-facebook-square"></i></span>
-                    <p class="text-white "> Universitas Nurul Jadid</p>
-                </a></div>
-            <div class="col-12 col-sm-6 col-md-3 text-center" data-aos="zoom-in-up" data-aos-duration="1000">
-                <a href="https://www.instagram.com/unujaofficial/?hl=id" target="blank" class="text-decoration-none">
-                    <span style="font-size: 40px; color: aliceblue;"><i class="fab fa-instagram"></i></span>
-                    <p class="text-white ">
-                        Universitas Nurul Jadid
-                    </p>
-                </a></div>
+    <div class="row ml-3 mr-3 justify-content-around">
+        <div class="col-sm-5 col-md-3 text-center align-self-center" data-aos="zoom-in-up" data-aos-duration="1000">
+            <a href="https://www.google.com/maps/place/Universitas+Nurul+Jadid/@-7.709959,113.498029,15z/data=!4m5!3m4!1s0x0:0xf34d3ceb3f9ddf2c!8m2!3d-7.709959!4d113.498029" target="blank" class="text-decoration-none">
+                <span style="font-size: 40px; color: aliceblue;"><i class="fas fa-map-marker-alt"></i></span>
+                <p class="text-white ">
+                    Jl. Pondok Pesantren Nurul Jadid, Karanganyar <br> Paiton, Probolinggo, Jawa Timur
+                </p>
+            </a>
         </div>
-        <footer id="sticky-footer" class="py-2 text-white shadow">
-            <div class="container text-center">
-                <small>Copyright &copy; Universitas Nurul Jadid </small>
+        <div class="col-sm-3 " data-aos="zoom-in-up" data-aos-duration="1000">
+            <center>
+                <img src="/img/logo.png" style="width: 100px; height: 100px; opacity: 0.8;">
+            </center>
+            <div class="text-white h6 font-weight-normal pt-2">
+                LPPK adalah suatu lembaga untuk menyalurkan kreatifitas untuk memompang minat dan bakat dari mahasiswa, dan diharapkan dapat bermanfaat bagi Universitas Nurul Jadid dan sekitarnya.
             </div>
-        </footer>
+            <div class="text-white justify-content-around pt-1 pb-5">
+                <a href="https://web.facebook.com/universitasnuruljadid" target="blank" class="text-decoration-none text-white pl-1"><i class="fab fa-facebook-square" style="font-size: 17px;"></i></a>
+                <a href="https://www.youtube.com/channel/UCq-VDezP1Vj4pEOYiPn3Qxw" target="blank" class="text-decoration-none text-white pl-1"><i class="fab fa-youtube" style="font-size: 17px;"></i></a>
+                <a href="https://www.instagram.com/unujaofficial/?hl=id" target="blank" class="text-decoration-none text-white pl-1"><i class="fab fa-instagram" style="font-size: 17px;"></i></a>
+                <a href="http://" target="blank" class="text-decoration-none text-white pl-1"><i class="fas fa-link " style="font-size: 17px;"></i></a>
+            </div>
+        </div>
+        <!-- <div class="col-12 col-sm-6 col-md-3 text-center" data-aos="zoom-in-up" data-aos-duration="1000">
+            <a href="https://www.youtube.com/channel/UCq-VDezP1Vj4pEOYiPn3Qxw" target="blank" class="text-decoration-none">
+                <span style="font-size: 40px; color: aliceblue;"><i class="fab fa-youtube"></i></span>
+                <p class="text-white ">
+                    Universitas Nurul Jadid
+                </p>
+            </a></div>
+        <div class="col-12 col-sm-6 col-md-3 text-center" data-aos="zoom-in-up" data-aos-duration="1000">
+            <a href="https://web.facebook.com/universitasnuruljadid" target="blank" class="text-decoration-none">
+                <span style="font-size: 40px; color: aliceblue;"><i class="fab fa-facebook-square"></i></span>
+                <p class="text-white "> Universitas Nurul Jadid</p>
+            </a></div>
+        <div class="col-12 col-sm-6 col-md-3 text-center" data-aos="zoom-in-up" data-aos-duration="1000">
+            <a href="https://www.instagram.com/unujaofficial/?hl=id" target="blank" class="text-decoration-none">
+                <span style="font-size: 40px; color: aliceblue;"><i class="fab fa-instagram"></i></span>
+                <p class="text-white ">
+                    Universitas Nurul Jadid
+                </p>
+            </a></div> -->
     </div>
+    <footer id="sticky-footer" class="py-2 text-white shadow">
+        <div class="container text-center">
+            <small>Copyright &copy; LPPK Universitas Nurul Jadid 2020 </small>
+        </div>
+    </footer>
+
 
 
     <!-- Tutorial Bootstrap -->
