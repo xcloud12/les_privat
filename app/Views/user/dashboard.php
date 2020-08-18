18b5edc02a5eee4bd7d87ef154067ea106f39c45
sutrisnob45 <sutrisnob45@gmail.com>
2020-08-18 12:38:54 +0700

    tampil nama di dasboard

--- app/Views/user/dashboard.php
+++ app/Views/user/dashboard.php
@@ -10,7 +10,7 @@
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Halaman Utama Admin</h1>
+                        <h1 class="h3 mb-0 text-gray-800">Selamat Datang <?= $nama ?></h1>
                     </div>
 
                     <!-- Content Row -->
